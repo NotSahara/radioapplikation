d3839f93d85b621843bda37cd4b7b5714daba23e
Sara <saramattisson@live.se>
2021-07-22 10:24:06 +0200

    Blandning av allt i uppstart

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+// import { Link } from "react-router-dom";
+import "../css/navbar.module.css";
+
+function Navbar() {
+  return(
+    <div>
+      <h1>Tomatradio</h1>
+    </div>
+  );
+}
+
+export default Navbar;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import "../css/global.module.css";
+
+function Home() {
+
+  return (
+  <div className="Testandet">
+    <h1>Det här är: "This is great! FROOOOOSTIES"</h1>
+  </div>
+  );
+}
+
+
+export default Home;
